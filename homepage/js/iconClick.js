d7ba8adca05435bd5a7b11daf2e7e9208480deb9
Meshi <kentiiyama@gmail.com>
2025-06-27 19:50:03 +0900

    add cecret.html

---
+++ homepage/js/iconClick.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const icon = document.getElementById("icon");
+  let clickCount = 0;
+  const threshold = 5; // 5回クリックでログインページへとぶ
+
+  icon.addEventListener("click", () => {
+    clickCount++;
+    if (clickCount >= threshold) {
+      window.location.href = "secret.html";
+    }
+  });
+});
