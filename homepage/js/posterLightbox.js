a86d8e49a5eb8dff0d4aece7776ce0456be3dfaa
Meshi <kentiiyama@gmail.com>
2025-08-25 11:17:54 +0900

    update poster light

--- homepage/js/posterLightbox.js
+++ homepage/js/posterLightbox.js
@@ -1,12 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
   const images = document.querySelectorAll(".poster-img");
   const lightbox = document.getElementById("lightbox");
   const lightboxImg = document.getElementById("lightboxImg");
   const closeBtn = document.querySelector(".lightbox .close");
 
+  // 画像クリックでライトボックスを開く
   images.forEach(img => {
     img.addEventListener("click", () => {
-      lightbox.style.display = "flex"; // フレックス表示に
-      lightboxImg.src = img.src;       // クリックした画像を表示
+      lightbox.style.display = "flex";
+      lightboxImg.src = img.src;
     });
   });
 
@@ -21,3 +23,4 @@
       lightbox.style.display = "none";
     }
   });
+});
